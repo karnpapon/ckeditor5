90fe5be9e8f2bc3b5f876e4e46fea95f8de9543f
karnpapon <karnpapon@gmail.com>
2020-06-12 18:36:23 +0700

    edited plugins list.

--- packages/ckeditor5-build-classic/src/ckeditor.js
+++ packages/ckeditor5-build-classic/src/ckeditor.js
@@ -8,28 +8,27 @@
 import ClassicEditorBase from '@ckeditor/ckeditor5-editor-classic/src/classiceditor';
 
 import Essentials from '@ckeditor/ckeditor5-essentials/src/essentials';
-import UploadAdapter from '@ckeditor/ckeditor5-adapter-ckfinder/src/uploadadapter';
+// import UploadAdapter from '@ckeditor/ckeditor5-adapter-ckfinder/src/uploadadapter';
 import Autoformat from '@ckeditor/ckeditor5-autoformat/src/autoformat';
 import Bold from '@ckeditor/ckeditor5-basic-styles/src/bold';
 import Italic from '@ckeditor/ckeditor5-basic-styles/src/italic';
 import Underline from '@ckeditor/ckeditor5-basic-styles/src/underline';
 import Strikethrough from '@ckeditor/ckeditor5-basic-styles/src/strikethrough';
 import BlockQuote from '@ckeditor/ckeditor5-block-quote/src/blockquote';
-import CKFinder from '@ckeditor/ckeditor5-ckfinder/src/ckfinder';
 import EasyImage from '@ckeditor/ckeditor5-easy-image/src/easyimage';
 import Heading from '@ckeditor/ckeditor5-heading/src/heading';
 import Image from '@ckeditor/ckeditor5-image/src/image';
 import ImageCaption from '@ckeditor/ckeditor5-image/src/imagecaption';
 import ImageStyle from '@ckeditor/ckeditor5-image/src/imagestyle';
 import ImageToolbar from '@ckeditor/ckeditor5-image/src/imagetoolbar';
-import ImageUpload from '@ckeditor/ckeditor5-image/src/imageupload';
+// import ImageUpload from '@ckeditor/ckeditor5-image/src/imageupload';
 import ImageResize from '@ckeditor/ckeditor5-image/src/imageresize';
 import Indent from '@ckeditor/ckeditor5-indent/src/indent';
 import Link from '@ckeditor/ckeditor5-link/src/link';
 import List from '@ckeditor/ckeditor5-list/src/list';
 import MediaEmbed from '@ckeditor/ckeditor5-media-embed/src/mediaembed';
-// import MediaToolbar from '@ckeditor/ckeditor5-media-embed/src/mediaembedtoolbar';
-// import MediaStyle from '@kimnagui/ckeditor5-media-align';
+import MediaToolbar from '@ckeditor/ckeditor5-media-embed/src/mediaembedtoolbar';
+import MediaStyle from '@kimnagui/ckeditor5-media-align';
 import Paragraph from '@ckeditor/ckeditor5-paragraph/src/paragraph';
 import PasteFromOffice from '@ckeditor/ckeditor5-paste-from-office/src/pastefromoffice';
 import Table from '@ckeditor/ckeditor5-table/src/table';
@@ -37,25 +36,16 @@ import TableToolbar from '@ckeditor/ckeditor5-table/src/tabletoolbar';
 import TextTransformation from '@ckeditor/ckeditor5-typing/src/texttransformation';
 
 import Plugin from '@ckeditor/ckeditor5-core/src/plugin';
-// import ButtonView from '@ckeditor/ckeditor5-ui/src/button/buttonview';
 import imageIcon from '@ckeditor/ckeditor5-core/theme/icons/image.svg';
 import { createDropdown } from '@ckeditor/ckeditor5-ui/src/dropdown/utils';
 import MediaFormView from '@ckeditor/ckeditor5-media-embed/src/ui/mediaformview';
-// import GFMDataProcessor from '@ckeditor/ckeditor5-markdown-gfm/src/gfmdataprocessor';
-
-// Simple plugin which loads the data processor.
-// function Markdown( editor ) {
-// 	editor.data.processor = new GFMDataProcessor( editor.editing.view.document );
-// }
 
 class InsertImage extends Plugin {
 	init() {
 		const editor = this.editor;
 
 		editor.ui.componentFactory.add( 'insertImage', locale => {
-			// const view = new ButtonView( locale );
 			const dropdown = createDropdown( locale );
-
 			const mediaForm = new MediaFormView( getFormValidators( editor.t ), editor.locale );
 
 			this._setUpDropdown( dropdown, mediaForm, editor );
@@ -122,28 +112,27 @@ export default class ClassicEditor extends ClassicEditorBase {}
 // Plugins to include in the build.
 ClassicEditor.builtinPlugins = [
 	Essentials,
-	UploadAdapter,
+	// UploadAdapter,
 	Autoformat,
 	Bold,
 	Italic,
 	Underline,
 	Strikethrough,
 	BlockQuote,
-	CKFinder,
 	EasyImage,
 	Heading,
 	Image,
 	ImageCaption,
 	ImageStyle,
 	ImageToolbar,
-	ImageUpload,
+	// ImageUpload,
 	ImageResize,
 	Indent,
 	Link,
 	List,
 	MediaEmbed,
-	// MediaToolbar,
-	// MediaStyle,
+	MediaToolbar,
+	MediaStyle,
 	InsertImage,
 	Paragraph,
 	PasteFromOffice,
@@ -178,9 +167,6 @@ ClassicEditor.defaultConfig = {
 			'redo'
 		]
 	},
-	ckfinder: {
-		openerMethod: 'popup'
-	},
 	image: {
 		styles: [
 			'full',
@@ -198,15 +184,15 @@ ClassicEditor.defaultConfig = {
 		]
 	},
 	mediaEmbed: {
-		previewsInData: true
-		// toolbar: [
-		// 	'mediaStyle:full',
-		// 	'|',
-		// 	'mediaStyle:alignLeft',
-		// 	'mediaStyle:alignCenter',
-		// 	'mediaStyle:alignRight'
-		// ],
-		// styles: [ 'full', 'alignLeft', 'alignCenter', 'alignRight' ],
+		previewsInData: true,
+		toolbar: [
+			'mediaStyle:full',
+			'|',
+			'mediaStyle:alignLeft',
+			'mediaStyle:alignCenter',
+			'mediaStyle:alignRight'
+		],
+		styles: [ 'full', 'alignLeft', 'alignCenter', 'alignRight' ]
 	},
 	table: {
 		contentToolbar: [
